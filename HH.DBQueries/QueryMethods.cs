12fb9961ad1e06374dba04ffda934ce8047d2557
Paula Holubek <paulaholubek@att.net>
2018-11-28 15:14:44 -0500

    Added logic for querying property results

--- HH.DBQueries/QueryMethods.cs
+++ HH.DBQueries/QueryMethods.cs
@@ -3,10 +3,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HH.ViewModels;
+using HH.DB.Models;
+using HH.DBQueries.DTOs;
 
 namespace HH.DBQueries
 {
     public class QueryMethods
     {
+        HousingHealthDB db = new HousingHealthDB();
+
+        public PropertyDTO GetPropertyInfo(string num, string street)
+        {
+            var propinfo = (from prop in db.Properties
+                        where prop.number == num && prop.street == street
+                        orderby prop.street, prop.number
+                        select new PropertyDTO
+                        {
+                            IsActive = prop.IsActive,
+                            CreatedByDate = prop.CreatedByDate,
+                            Parcel = prop.parcel,
+                            Date = prop.date,
+                            Towner = prop.towner,
+                            Lsaleamt = prop.lsaleamt,
+                            Number = prop.number,
+                            Street = prop.street,
+                            Tract10 = prop.tract10,
+                            BLOCK10 = prop.BLOCK10,
+                            BLOCKGR10 = prop.BLOCKGR10,
+                            Pclass = prop.pclass,
+                            Luc = prop.luc,
+                            Luc_descr = prop.luc_descr,
+                            Yrbuilt = prop.yrbuilt,
+                            MAILNAME = prop.MAILNAME,
+                            Mailname1 = prop.mailname1,
+                            MAIL_STREET_NUMBER = prop.MAIL_STREET_NUMBER,
+                            MAIL_STREET_DIRECTION = prop.MAIL_STREET_DIRECTION,
+                            MAIL_STREET_NAME = prop.MAIL_STREET_NAME,
+                            MAIL_STREET_SUFFIX = prop.MAIL_STREET_SUFFIX,
+                            MAIL_CITY = prop.MAIL_CITY,
+                            MAIL_STATE = prop.MAIL_STATE,
+                            MAIL_ZIPCODE = prop.MAIL_ZIPCODE,
+                            TOTAL_NET_DELQ_BALANCE = prop.TOTAL_NET_DELQ_BALANCE
+                        }).First();
+
+                return propinfo;
+        }
+
+
     }
 }
+//List<PropertiesViewModels> propertiesVM = propinfo.Select(item => new PropertiesViewModels()
+//{
+//    IsActive = item.IsActive,
+//    CreatedByDate = item.CreatedByDate,
+//    Parcel = item.Parcel,
+//    Date = item.Date,
+//    Towner = item.Towner,
+//    Lsaleamt = item.Lsaleamt,
+//    Number = item.Number,
+//    Street = item.Street,
+//    Tract10 = item.Tract10,
+//    BLOCK10 = item.BLOCK10,
+//    BLOCKGR10 = item.BLOCKGR10,
+//    Pclass = item.Pclass,
+//    Luc = item.Luc,
+//    Luc_descr = item.Luc_descr,
+//    Yrbuilt = item.Yrbuilt,
+//    MAILNAME = item.MAILNAME,
+//    Mailname1 = item.Mailname1,
+//    MAIL_STREET_NUMBER = item.MAIL_STREET_NUMBER,
+//    MAIL_STREET_DIRECTION = item.MAIL_STREET_DIRECTION,
+//    MAIL_STREET_NAME = item.MAIL_STREET_NAME,
+//    MAIL_STREET_SUFFIX = item.MAIL_STREET_SUFFIX,
+//    MAIL_CITY = item.MAIL_CITY,
+//    MAIL_STATE = item.MAIL_STATE,
+//    MAIL_ZIPCODE = item.MAIL_ZIPCODE,
+//    TOTAL_NET_DELQ_BALANCE = item.TOTAL_NET_DELQ_BALANCE
+//}).ToList();
---
+++ HH/ViewModels/PropertiesViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HH.ViewModels
+{
+    public class PropertiesViewModels
+    {
+        public bool IsActive { get; set; }
+        public DateTime CreatedByDate { get; set; }
+        public int User_ID { get; set; }
+        public string Parcel { get; set; }
+        public DateTime Date { get; set; }
+        public string Towner { get; set; }
+        public decimal Lsaleamt { get; set; }
+        public string Number { get; set; }
+        public string Street { get; set; }
+        public string Tract10 { get; set; }
+        public string BLOCK10 { get; set; }
+        public string BLOCKGR10 { get; set; }
+        public string Pclass { get; set; }
+        public string Luc { get; set; }
+        public string Luc_descr { get; set; }
+        public int Yrbuilt { get; set; }
+        public string MAILNAME { get; set; }
+        public string Mailname1 { get; set; }
+        public string MAIL_STREET_NUMBER { get; set; }
+        public string MAIL_STREET_DIRECTION { get; set; }
+        public string MAIL_STREET_NAME { get; set; }
+        public string MAIL_STREET_SUFFIX { get; set; }
+        public string MAIL_CITY { get; set; }
+        public string MAIL_STATE { get; set; }
+        public string MAIL_ZIPCODE { get; set; }
+        public string TOTAL_NET_DELQ_BALANCE { get; set; }
+    }
+}
